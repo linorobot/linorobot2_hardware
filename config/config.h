3f8a12d48e8c6dfbd9480f6da4b76aa404007906
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-08-18 20:15:32 +0800

    added custom robots

--- config/config.h
+++ config/config.h
@@ -2,11 +2,15 @@
     #include "custom/beebo_config.h"
 #endif
 
+#ifdef USE_HEX_CONFIG
+    #include "custom/hex_config.h"
+#endif
+
 #ifdef USE_DEV_CONFIG
     #include "custom/dev_config.h"
 #endif
 
-#if !defined (USE_BEEBO_CONFIG) && !defined (USE_DEV_CONFIG)
+#if !defined (USE_BEEBO_CONFIG) && !defined (USE_DEV_CONFIG) && !defined (USE_HEX_CONFIG)
     #include "lino_base_config.h"
 #endif
 
