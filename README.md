e1ca937760cd4c8479c7f5c3866f463172485b25
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-09-03 00:39:22 +0800

    added sample link on how to invert motor and encoder

--- README.md
+++ README.md
@@ -219,7 +219,7 @@ Constants' Meaning:
 
 - **MOTORX_INV** - Flag used to invert the direction of the motor. More on that later.
 
-Teensy 3.x and 4.x have different mapping of PWM pins, read the notes beside each pin assignment in [lino_base_config.h](https://github.com/linorobot/linorobot2_prototype/blob/master/config/lino_base_config.h#L108) carefully to avoid connecting your driver's PWM pin to a non PWM pin on Teensy. 
+Teensy 3.x and 4.x have different mapping of PWM pins, read the notes beside each pin assignment in [lino_base_config.h](https://github.com/linorobot/linorobot2_prototype/blob/master/config/lino_base_config.h#L109) carefully to avoid connecting your driver's PWM pin to a non PWM pin on Teensy. 
 
 ## 5. Motor and Encoder Checks
 Before proceeding, **ensure that your robot is elevated and the wheels aren't touching the ground**. 
@@ -247,7 +247,7 @@ Start spinning the motors by running:
 
 On the terminal type `spin` and press the enter key.
 
-The wheels will spin one by one for 10 seconds from Motor1 to Motor4. Check if each wheel's direction is spinning **forward** and take note of the motors that are spinning in the opposite direction. Set MOTORX_INV constant in lino_base_config.h to `true` to invert the motor's direction. Reupload the calibration firmware once you're done. Press `Ctrl` + `a` + `d` to exit the screen terminal.
+The wheels will spin one by one for 10 seconds from Motor1 to Motor4. Check if each wheel's direction is spinning **forward** and take note of the motors that are spinning in the opposite direction. Set MOTORX_INV constant in [lino_base_config.h](https://github.com/linorobot/linorobot2_prototype/blob/master/config/lino_base_config.h#L71-L74) to `true` to invert the motor's direction. Reupload the calibration firmware once you're done. Press `Ctrl` + `a` + `d` to exit the screen terminal.
 
     cd linorobot2_prototype/calibration
     pio run --target upload -e <your_teensy_board>
@@ -260,7 +260,7 @@ Open your terminal and run:
 
 Type `sample` and press the enter key. Verify if all the wheels are spinning **forward**. Redo the previous step (5.1) if there are motors still spinning in the opposite direction.
 
-You'll see a summary of the total encoder readings and counts per revolution after the motors have been sampled. If you see any negative number in the MOTOR ENCODER READINGS section, invert the encoder pin by setting `MOTORX_ENCODER_INV` in lino_base_config.h to `true`. Reupload the calibration firmware to check if the encoder pins have been reconfigured properly:
+You'll see a summary of the total encoder readings and counts per revolution after the motors have been sampled. If you see any negative number in the MOTOR ENCODER READINGS section, invert the encoder pin by setting `MOTORX_ENCODER_INV` in [lino_base_config.h](https://github.com/linorobot/linorobot2_prototype/blob/master/config/lino_base_config.h#L65-L68) to `true`. Reupload the calibration firmware to check if the encoder pins have been reconfigured properly:
 
     cd linorobot2_prototype/calibration
     pio run --target upload -e <your_teensy_board>
@@ -270,7 +270,7 @@ Type `sample` and press the enter key. Verify if all encoder values are now **po
 
 ### 5.3 Counts Per Revolution
 
-On the previous instruction where you check the encoder reads for each motor, you'll see that there's also COUNTES PER REVOLUTION values printed on the screen. If you have defined `MOTOR_OPERATING_VOLTAGE` and `MOTOR_POWER_MEASURED_VOLTAGE`, you can assign these values to `COUNTS_PER_REVX` constants in lino_base_config.h to have a more accurate model of the encoder.
+On the previous instruction where you check the encoder reads for each motor, you'll see that there's also COUNTES PER REVOLUTION values printed on the screen. If you have defined `MOTOR_OPERATING_VOLTAGE` and `MOTOR_POWER_MEASURED_VOLTAGE`, you can assign these values to `COUNTS_PER_REVX` constants in [lino_base_config.h](https://github.com/linorobot/linorobot2_prototype/blob/master/config/lino_base_config.h#L55-L58) to have a more accurate model of the encoder.
 
 ### 5.4 Troubleshooting Guide
 
--- config/lino_base_config.h
+++ config/lino_base_config.h
@@ -61,44 +61,48 @@ ROBOT ORIENTATION
 #define PWM_BITS 8                          // PWM Resolution of the microcontroller
 #define PWM_FREQUENCY 20000                 // PWM Frequency
 
-/// ENCODER PINS
+// INVERT ENCODER COUNTS
+#define MOTOR1_ENCODER_INV false 
+#define MOTOR2_ENCODER_INV false 
+#define MOTOR3_ENCODER_INV false 
+#define MOTOR4_ENCODER_INV false 
+
+// INVERT MOTOR DIRECTIONS
+#define MOTOR1_INV false
+#define MOTOR2_INV false
+#define MOTOR3_INV false
+#define MOTOR4_INV false
+
+// ENCODER PINS
 #define MOTOR1_ENCODER_A 14
 #define MOTOR1_ENCODER_B 15 
-#define MOTOR1_ENCODER_INV false 
 
 #define MOTOR2_ENCODER_A 11
 #define MOTOR2_ENCODER_B 12 
-#define MOTOR2_ENCODER_INV false 
 
 #define MOTOR3_ENCODER_A 17
 #define MOTOR3_ENCODER_B 16 
-#define MOTOR3_ENCODER_INV false 
 
 #define MOTOR4_ENCODER_A 9
 #define MOTOR4_ENCODER_B 10
-#define MOTOR4_ENCODER_INV false 
 
-//MOTOR PINS
+// MOTOR PINS
 #ifdef USE_GENERIC_2_IN_MOTOR_DRIVER
   #define MOTOR1_PWM 21 //Pin no 21 is not a PWM pin on Teensy 4.x, you can swap it with pin no 1 instead.
   #define MOTOR1_IN_A 20
   #define MOTOR1_IN_B 1 
-  #define MOTOR1_INV false
 
   #define MOTOR2_PWM 5
   #define MOTOR2_IN_A 6
   #define MOTOR2_IN_B 8
-  #define MOTOR2_INV false
 
   #define MOTOR3_PWM 22
   #define MOTOR3_IN_A 23
   #define MOTOR3_IN_B 0
-  #define MOTOR3_INV false
 
   #define MOTOR4_PWM 4
   #define MOTOR4_IN_A 3
   #define MOTOR4_IN_B 2
-  #define MOTOR4_INV false
 
   #define PWM_MAX pow(2, PWM_BITS) - 1
   #define PWM_MIN -PWM_MAX
@@ -108,22 +112,18 @@ ROBOT ORIENTATION
   #define MOTOR1_PWM 21 //Pin no 21 is not a PWM pin on Teensy 4.x, you can use pin no 1 instead.
   #define MOTOR1_IN_A 20
   #define MOTOR1_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR1_INV false
 
   #define MOTOR2_PWM 5
   #define MOTOR2_IN_A 6
   #define MOTOR2_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR2_INV false
 
   #define MOTOR3_PWM 22
   #define MOTOR3_IN_A 23
   #define MOTOR3_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR3_INV false
 
   #define MOTOR4_PWM 4
   #define MOTOR4_IN_A 3
   #define MOTOR4_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR4_INV false
 
   #define PWM_MAX pow(2, PWM_BITS) - 1
   #define PWM_MIN -PWM_MAX
@@ -133,22 +133,18 @@ ROBOT ORIENTATION
   #define MOTOR1_PWM -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR1_IN_A 21 // Pin no 21 is not a PWM pin on Teensy 4.x, you can use pin no 1 instead.
   #define MOTOR1_IN_B 20 // Pin no 20 is not a PWM pin on Teensy 4.x, you can use pin no 0 instead.
-  #define MOTOR1_INV false
 
   #define MOTOR2_PWM -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR2_IN_A 5
   #define MOTOR2_IN_B 6
-  #define MOTOR2_INV false
 
   #define MOTOR3_PWM -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR3_IN_A 22
   #define MOTOR3_IN_B 23
-  #define MOTOR3_INV false
 
   #define MOTOR4_PWM -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR4_IN_A 4
   #define MOTOR4_IN_B 3
-  #define MOTOR4_INV false
 
   #define PWM_MAX pow(2, PWM_BITS) - 1
   #define PWM_MIN -PWM_MAX
@@ -158,27 +154,21 @@ ROBOT ORIENTATION
   #define MOTOR1_PWM 21 //Pin no 21 is not a PWM pin on Teensy 4.x. You can use pin no 1 instead.
   #define MOTOR1_IN_A -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR1_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR1_INV false
 
   #define MOTOR2_PWM 5
   #define MOTOR2_IN_A -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR2_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR2_INV false
 
   #define MOTOR3_PWM 22 
   #define MOTOR3_IN_A -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR3_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR3_INV false
 
   #define MOTOR4_PWM 4
   #define MOTOR4_IN_A -1 //DON'T TOUCH THIS! This is just a placeholder
   #define MOTOR4_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
-  #define MOTOR4_INV false
 
   #define PWM_MAX 400
   #define PWM_MIN -PWM_MAX
 #endif
-
-#define STEERING_PIN 7
 
 #endif
