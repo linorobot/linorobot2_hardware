1384bc238ab83dddbb1ce729a646990cca19641d
gssabu <87628438+gssabu@users.noreply.github.com>
2022-04-24 19:47:24 -0500

    Update lino_base_config.h

--- config/lino_base_config.h
+++ config/lino_base_config.h
@@ -29,10 +29,10 @@
 // #define USE_ESC_MOTOR_DRIVER            // Motor ESC for brushless motors
 
 //uncomment the IMU you're using
-#define USE_GY85_IMU
+// #define USE_GY85_IMU
 // #define USE_MPU6050_IMU
 // #define USE_MPU9150_IMU
-// #define USE_MPU9250_IMU
+#define USE_MPU9250_IMU
 
 #define K_P 0.6                             // P constant
 #define K_I 0.8                             // I constant
@@ -58,19 +58,19 @@ ROBOT ORIENTATION
 */
 
 //define your robot' specs here
-#define MOTOR_MAX_RPM 140                   // motor's max RPM          
-#define MAX_RPM_RATIO 0.85                  // max RPM allowed for each MAX_RPM_ALLOWED = MOTOR_MAX_RPM * MAX_RPM_RATIO          
-#define MOTOR_OPERATING_VOLTAGE 24          // motor's operating voltage (used to calculate max RPM)
+#define MOTOR_MAX_RPM 100                   // motor's max RPM          
+#define MAX_RPM_RATIO 0.75                  // max RPM allowed for each MAX_RPM_ALLOWED = MOTOR_MAX_RPM * MAX_RPM_RATIO          
+#define MOTOR_OPERATING_VOLTAGE 12          // motor's operating voltage (used to calculate max RPM)
 #define MOTOR_POWER_MAX_VOLTAGE 12          // max voltage of the motor's power source (used to calculate max RPM)
 #define MOTOR_POWER_MEASURED_VOLTAGE 12     // current voltage reading of the power connected to the motor (used for calibration)
-#define COUNTS_PER_REV1 144000              // wheel1 encoder's no of ticks per rev
-#define COUNTS_PER_REV2 144000              // wheel2 encoder's no of ticks per rev
+#define COUNTS_PER_REV1 3576              // wheel1 encoder's no of ticks per rev
+#define COUNTS_PER_REV2 3576              // wheel2 encoder's no of ticks per rev
 #define COUNTS_PER_REV3 144000              // wheel3 encoder's no of ticks per rev
 #define COUNTS_PER_REV4 144000              // wheel4 encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.152                // wheel's diameter in meters
-#define LR_WHEELS_DISTANCE 0.271            // distance between left and right wheels
-#define PWM_BITS 10                          // PWM Resolution of the microcontroller
-#define PWM_FREQUENCY 20000                 // PWM Frequency
+#define WHEEL_DIAMETER 0.70                // wheel's diameter in meters
+#define LR_WHEELS_DISTANCE 0.170            // distance between left and right wheels
+#define PWM_BITS 12                          // PWM Resolution of the microcontroller
+#define PWM_FREQUENCY 25000                 // PWM Frequency
 
 // INVERT ENCODER COUNTS
 #define MOTOR1_ENCODER_INV false 
@@ -85,31 +85,31 @@ ROBOT ORIENTATION
 #define MOTOR4_INV false
 
 // ENCODER PINS
-#define MOTOR1_ENCODER_A 14
-#define MOTOR1_ENCODER_B 15 
+#define MOTOR1_ENCODER_A 40
+#define MOTOR1_ENCODER_B 41 
 
-#define MOTOR2_ENCODER_A 11
-#define MOTOR2_ENCODER_B 12 
+#define MOTOR2_ENCODER_A 0
+#define MOTOR2_ENCODER_B 1 
 
-#define MOTOR3_ENCODER_A 17
-#define MOTOR3_ENCODER_B 16 
+#define MOTOR3_ENCODER_A 21
+#define MOTOR3_ENCODER_B 22 
 
-#define MOTOR4_ENCODER_A 9
-#define MOTOR4_ENCODER_B 10
+#define MOTOR4_ENCODER_A 23
+#define MOTOR4_ENCODER_B 24
 
 // MOTOR PINS
 #ifdef USE_GENERIC_2_IN_MOTOR_DRIVER
-  #define MOTOR1_PWM 21 //Pin no 21 is not a PWM pin on Teensy 4.x, you can swap it with pin no 1 instead.
-  #define MOTOR1_IN_A 20
-  #define MOTOR1_IN_B 1 
+  #define MOTOR1_PWM 7 //Pin no 21 is not a PWM pin on Teensy 4.x, you can swap it with pin no 1 instead.
+  #define MOTOR1_IN_A 6
+  #define MOTOR1_IN_B 5 
 
-  #define MOTOR2_PWM 5
-  #define MOTOR2_IN_A 6
-  #define MOTOR2_IN_B 8
+  #define MOTOR2_PWM 2
+  #define MOTOR2_IN_A 4
+  #define MOTOR2_IN_B 3
 
-  #define MOTOR3_PWM 22
-  #define MOTOR3_IN_A 23
-  #define MOTOR3_IN_B 0
+  #define MOTOR3_PWM 28
+  #define MOTOR3_IN_A 29
+  #define MOTOR3_IN_B 30
 
   #define MOTOR4_PWM 4
   #define MOTOR4_IN_A 3
