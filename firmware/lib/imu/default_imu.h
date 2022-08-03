96a730c8048d6b5f5e3729ed326134a5d1c1e985
samuk <sam@bristolwireless.net>
2022-08-03 13:46:48 +0100

    Commented out class MPU9150IMU:

--- firmware/lib/imu/default_imu.h
+++ firmware/lib/imu/default_imu.h
@@ -161,66 +161,66 @@ class MPU6050IMU: public IMUInterface
         }
 };
 
-class MPU9150IMU: public IMUInterface 
-{
-    private:
-        const float accel_scale_ = 1 / 16384.0;
-        const float gyro_scale_ = 1 / 131.0;
+//class MPU9150IMU: public IMUInterface 
+//{
+//    private:
+//        const float accel_scale_ = 1 / 16384.0;
+//        const float gyro_scale_ = 1 / 131.0;
 
-        MPU9150 accelerometer_;
-        MPU9150 gyroscope_;
+//        MPU9150 accelerometer_;
+//        MPU9150 gyroscope_;
 
-        geometry_msgs__msg__Vector3 accel_;
-        geometry_msgs__msg__Vector3 gyro_;
+//        geometry_msgs__msg__Vector3 accel_;
+//        geometry_msgs__msg__Vector3 gyro_;
 
-    public:
-        MPU9150IMU()
+//    public:
+//        MPU9150IMU()
         {
         }
 
-        bool startSensor() override
-        {
-            Wire.begin();
-            bool ret;
-            accelerometer_.initialize();
-            ret = accelerometer_.testConnection();
-            if(!ret)
-                return false;
+//        bool startSensor() override
+//        {
+//            Wire.begin();
+//            bool ret;
+//            accelerometer_.initialize();
+//            ret = accelerometer_.testConnection();
+         //   if(!ret)
+       //         return false;
 
-            gyroscope_.initialize();
-            ret = gyroscope_.testConnection();
-            if(!ret)
-                return false;
+     //       gyroscope_.initialize();
+    //        ret = gyroscope_.testConnection();
+      //      if(!ret)
+      //          return false;
 
-            return true;
-        }
+    //        return true;
+  //      }
 
-        geometry_msgs__msg__Vector3 readAccelerometer() override
-        {
-            int16_t ax, ay, az;
+    //    geometry_msgs__msg__Vector3 readAccelerometer() override
+   //     {
+  //          int16_t ax, ay, az;
             
-            accelerometer_.getAcceleration(&ax, &ay, &az);
+//            accelerometer_.getAcceleration(&ax, &ay, &az);
+//
+//            accel_.x = ax * (double) accel_scale_ * g_to_accel_;
+//            accel_.y = ay * (double) accel_scale_ * g_to_accel_;
+//            accel_.z = az * (double) accel_scale_ * g_to_accel_;
 
-            accel_.x = ax * (double) accel_scale_ * g_to_accel_;
-            accel_.y = ay * (double) accel_scale_ * g_to_accel_;
-            accel_.z = az * (double) accel_scale_ * g_to_accel_;
+     //       return accel_;
+   //     }
 
-            return accel_;
-        }
+ //       geometry_msgs__msg__Vector3 readGyroscope() override
+ //       {
+ //           int16_t gx, gy, gz;
 
-        geometry_msgs__msg__Vector3 readGyroscope() override
-        {
-            int16_t gx, gy, gz;
-
-            gyroscope_.getRotation(&gx, &gy, &gz);
-
-            gyro_.x = gx * (double) gyro_scale_ * DEG_TO_RAD;
-            gyro_.y = gy * (double) gyro_scale_ * DEG_TO_RAD;
-            gyro_.z = gz * (double) gyro_scale_ * DEG_TO_RAD;
-
-            return gyro_;
-        }
-};
+//            gyroscope_.getRotation(&gx, &gy, &gz);
+//
+ //           gyro_.x = gx * (double) gyro_scale_ * DEG_TO_RAD;
+//            gyro_.y = gy * (double) gyro_scale_ * DEG_TO_RAD;
+//            gyro_.z = gz * (double) gyro_scale_ * DEG_TO_RAD;
+//
+//            return gyro_;
+//        }
+//};
 
 class MPU9250IMU: public IMUInterface 
 {
