2b964341292e1969535d39c46f72aed162394b3f
samuk <sam@bristolwireless.net>
2022-08-10 10:21:06 +0100

    Added I2C

--- firmware/lib/motor/default_motor.h
+++ firmware/lib/motor/default_motor.h
@@ -20,6 +20,52 @@
 
 #include "motor_interface.h"
 
+#include "SimpleFOCDrivers.h"
+#include "comms/i2c/I2CCommander.h"
+
+
+
+class I2CCOMMANDER: public MotorInterface
+
+// commander instance
+uint8_t i2c_addr = 0x60;  // can be anything you choose
+I2CCommander commander;
+// interrupt callbacks
+void onReceive(int numBytes) { commander.onReceive(numBytes); }
+void onRequest() { commander.onRequest(); }
+
+
+// ... other variables, like sensor, etc...
+BLDCMotor motor = BLDCMotor(POLE_PAIRS);
+
+
+void setup() {
+    
+    // ...other setup code
+
+    Wire.setClock(400000);          // use same speed on controller device
+    Wire.begin(i2c_addr, true);     // initialize i2c in target mode
+    commander.addMotor(&motor);     // add a motor
+    //commander.addMotor(&motor2);  // you could add more than one motor
+    commander.init(i2c_addr);       // initialize commander
+    Wire.onReceive(onReceive);      // connect the interrupt handlers
+    Wire.onRequest(onRequest);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Generic2: public MotorInterface
 {
     private:
