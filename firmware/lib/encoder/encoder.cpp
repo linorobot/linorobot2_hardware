cb4885d78d3cd270e6b21c9331d6f7415bdf4489
Thomas Chou <thomas@wytron.com.tw>
2023-05-19 18:00:08 -0700

    use esp32 pcnt module for encoder
    The esp32 supports 64 bits pulse counters. No interrupt is required.

--- firmware/lib/encoder/encoder.cpp
+++ firmware/lib/encoder/encoder.cpp
@@ -1,10 +1,11 @@
 
 #include "encoder.h"
 
+#ifndef ESP32
 // Yes, all the code is in the header file, to provide the user
 // configure options with #define (before they include it), and
 // to facilitate some crafty optimizations!
 
 Encoder_internal_state_t * Encoder::interruptArgs[];
-
+#endif
 
