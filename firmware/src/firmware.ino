349a0589a50919506ef6e9b7fbc0e35f7801b4e1
Thomas Chou <thomas@wytron.com.tw>
2023-05-22 11:04:42 -0700

    add ArduinoOTA support to config

--- firmware/src/firmware.ino
+++ firmware/src/firmware.ino
@@ -35,6 +35,10 @@
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include "encoder.h"
 
+#ifdef USE_ARDUINO_OTA
+#include <ArduinoOTA.h>
+#endif
+
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){rclErrorLoop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 #define EXECUTE_EVERY_N_MS(MS, X)  do { \
@@ -116,6 +120,51 @@ void setup()
 #else
     set_microros_serial_transports(Serial);
 #endif
+#ifdef USE_ARDUINO_OTA
+    // Port defaults to 3232
+    // ArduinoOTA.setPort(3232);
+
+    // Hostname defaults to esp3232-[MAC]
+    // ArduinoOTA.setHostname("myesp32");
+
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    ArduinoOTA
+      .onStart([]() {
+	  String type;
+	  if (ArduinoOTA.getCommand() == U_FLASH)
+	    type = "sketch";
+	  else // U_SPIFFS
+	    type = "filesystem";
+
+	  // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+	  Serial.println("Start updating " + type);
+	})
+      .onEnd([]() {
+	  Serial.println("\nEnd");
+	})
+      .onProgress([](unsigned int progress, unsigned int total) {
+	  Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+	})
+      .onError([](ota_error_t error) {
+	  Serial.printf("Error[%u]: ", error);
+	  if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+	  else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+	  else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+	  else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+	  else if (error == OTA_END_ERROR) Serial.println("End Failed");
+	});
+
+    ArduinoOTA.begin();
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+#endif
 }
 
 void loop() {
@@ -145,6 +194,9 @@ void loop() {
         default:
             break;
     }
+#ifdef USE_ARDUINO_OTA
+    ArduinoOTA.handle();
+#endif
 }
 
 void controlCallback(rcl_timer_t * timer, int64_t last_call_time) 
@@ -341,6 +393,9 @@ void rclErrorLoop()
     while(true)
     {
         flashLED(2);
+#ifdef USE_ARDUINO_OTA
+	ArduinoOTA.handle();
+#endif
     }
 }
 
