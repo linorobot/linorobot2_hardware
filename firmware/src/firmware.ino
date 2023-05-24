1ec33489766f099d328a4ac181fd826c825f36bf
Thomas Chou <thomas@wytron.com.tw>
2023-05-24 11:35:55 -0700

    syncTime current time should be queried after synced with agent

--- firmware/src/firmware.ino
+++ firmware/src/firmware.ino
@@ -508,11 +508,10 @@ void publishData()
 void syncTime()
 {
     // get the current time from the agent
-    unsigned long now = millis();
     RCCHECK(rmw_uros_sync_session(10));
     unsigned long long ros_time_ms = rmw_uros_epoch_millis(); 
     // now we can find the difference between ROS time and uC time
-    time_offset = ros_time_ms - now;
+    time_offset = ros_time_ms - millis();
 }
 
 struct timespec getTime()
