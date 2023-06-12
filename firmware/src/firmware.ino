30df0f6951242f5de49cb9e4f7d33bca86d3c96e
Thomas Chou <thomas@wytron.com.tw>
2023-06-12 12:27:23 -0700

    move ArduinoOTA to lib/wifi

--- firmware/src/firmware.ino
+++ firmware/src/firmware.ino
@@ -43,10 +43,8 @@
 #include "range.h"
 #include "lidar.h"
 #include "wifis.h"
+#include "ota.h"
 
-#ifdef USE_ARDUINO_OTA
-#include <ArduinoOTA.h>
-#endif
 #ifdef WDT_TIMEOUT
 #include <esp_task_wdt.h>
 #endif
@@ -176,6 +174,7 @@ void setup()
     esp_task_wdt_add(NULL); //add current thread to WDT watch
 #endif
     initWifis();
+    initOta();
 
     bool imu_ok = imu.init();
     if(!imu_ok)
@@ -194,48 +193,6 @@ void setup()
     set_microros_net_transports(AGENT_IP, AGENT_PORT);
 #else
     set_microros_serial_transports(Serial);
-#endif
-#ifdef USE_ARDUINO_OTA
-    // Port defaults to 3232
-    // ArduinoOTA.setPort(3232);
-
-    // Hostname defaults to esp3232-[MAC]
-    // ArduinoOTA.setHostname("myesp32");
-
-    // No authentication by default
-    // ArduinoOTA.setPassword("admin");
-
-    // Password can be set with it's md5 value as well
-    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-    ArduinoOTA
-      .onStart([]() {
-	  String type;
-	  if (ArduinoOTA.getCommand() == U_FLASH)
-	    type = "sketch";
-	  else // U_SPIFFS
-	    type = "filesystem";
-
-	  // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-	  Serial.println("Start updating " + type);
-	})
-      .onEnd([]() {
-	  Serial.println("\nEnd");
-	})
-      .onProgress([](unsigned int progress, unsigned int total) {
-	  Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-	})
-      .onError([](ota_error_t error) {
-	  Serial.printf("Error[%u]: ", error);
-	  if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-	  else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-	  else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-	  else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-	  else if (error == OTA_END_ERROR) Serial.println("End Failed");
-	});
-
-    ArduinoOTA.begin();
 #endif
     syslog(LOG_INFO, "%s Ready %lu", __FUNCTION__, millis());
 }
@@ -271,9 +228,7 @@ void loop() {
             break;
     }
     runWifis();
-#ifdef USE_ARDUINO_OTA
-    ArduinoOTA.handle();
-#endif
+    runOta();
 #ifdef WDT_TIMEOUT
     esp_task_wdt_reset();
 #endif
@@ -589,9 +544,7 @@ void rclErrorLoop()
     while(true)
     {
         flashLED(2);
-#ifdef USE_ARDUINO_OTA
-	ArduinoOTA.handle();
-#endif
+	runOta();
     }
 }
 
