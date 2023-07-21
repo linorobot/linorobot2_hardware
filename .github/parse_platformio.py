d2d54de087a25a60c967586e8423960be5a480a7
Atticus Russell <atticusrussell@gmail.com>
2023-07-21 12:06:34 -0400

    added comments

--- .github/parse_platformio.py
+++ .github/parse_platformio.py
@@ -1,13 +1,18 @@
 import configparser
 import json
 
+""" This script parses the platformio.ini file and outputs a JSON array of environments to be used by GitHub Actions."""
+
+# Parse the platformio.ini file
 config = configparser.ConfigParser()
 config.read('firmware/platformio.ini')
 
+# Extract the environment names from the sections
 environments = []
 for section in config.sections():
     if section.startswith("env:"):
         env_name = section.split("env:")[1]
         environments.append({"env": env_name})
 
+# Output the environments as a JSON array
 print(json.dumps(environments))
